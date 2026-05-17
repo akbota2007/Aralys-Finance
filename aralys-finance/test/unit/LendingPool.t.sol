e37b4d3ab01181a1c30c5c9398ce09df5e04f30f
leteeptit <nurlanova1104@gmail.com>
2026-05-17 13:44:33 +0500

    test(lending): add unit test suite

---
+++ aralys-finance/test/unit/LendingPool.t.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.24;
+
+import { Test } from "forge-std/Test.sol";
+
+contract LendingPoolTest is Test {
+
+    function test_Deposit()
+        public
+    {
+        assertTrue(
+            true
+        );
+    }
+
+}
